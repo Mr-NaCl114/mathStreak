8516c4c1efe8ee3240fe363f679909929bca73d8
Mr-NaCl114 <2054105384@qq.com>
2026-02-12 11:13:58 +0800

    first

--- mathStreak-web/src/test/java/com/lods/mathstreakweb/MathStreakWebApplicationTests.java
+++ mathStreak-web/src/test/java/com/lods/mathstreakweb/MathStreakWebApplicationTests.java
@@ -1,10 +1,10 @@
-package com.lods.mathstreakmvc;
+package com.lods.mathstreakweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MathStreakMvcApplicationTests {
+class MathStreakWebApplicationTests {
 
     @Test
     void contextLoads() {
