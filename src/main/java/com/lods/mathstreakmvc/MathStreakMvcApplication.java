be534aaaedf765311f45e91fcf493e97c4769d45
Mr-NaCl114 <2054105384@qq.com>
2026-02-12 10:50:43 +0800

    first

---
+++ src/main/java/com/lods/mathstreakmvc/MathStreakMvcApplication.java
@@ -0,0 +1,13 @@
+package com.lods.mathstreakmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathStreakMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathStreakMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lods/mathstreakmvc/MathStreakMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lods.mathstreakmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathStreakMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
